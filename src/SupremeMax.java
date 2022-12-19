8226f9abdb9ab79385a8c97610e74ba33eb7981c
Smile KID <90183035+PhuthiM@users.noreply.github.com>
2022-12-20 04:33:23 +0700

    Update SupremeMax.java

--- src/SupremeMax.java
+++ src/SupremeMax.java
@@ -5,25 +5,45 @@ public class SupremeMax {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        final int n = sc.nextInt();
         int[] arr = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; ++i) {
             arr[i] = sc.nextInt();
         }
+        int supMax = Integer.MIN_VALUE; // ค่า SupremeMax
+        int intervalMax = Integer.MIN_VALUE; // ค่าสูงสุดในช่วง
+        int interStart = 1; // อินเด็กซ์เริ่มของช่วงปัจจุบันที่กำลังสำรวจ
+        int bestStart = -1; // อินเด็กซ์เริ่มต้นของช่วงที่ค่าสูงที่สุด
+        int bestEnd = -1; // อินเด็กซ์สุดท้ายของช่วงที่ค่าสูงที่สุด
+        boolean zero = false;
+        // เริ่มการสำรวจและเก็บข้อมูลในแต่ละช่วง (เป็นลูปยาวสักหน่อย)
 
-        int[] max1 = new int[n + 1];
-        int count = 0;
-        int max = 0;
-        int startMax = 0;
-        int lastMax = 0;
         for (int i = 1; i <= n; i++) {
+
             if (arr[i] != 0) {
-                startMax = i;
-                for (int j = i; j <= n; j++) {
-                    
+                if (arr[i] > intervalMax) {
+                    intervalMax = arr[i];
                 }
+                zero=false;
+            } else if (arr[i] == 0 && !zero) {
+                System.out.print(intervalMax + " ");
+                // System.out.println(interStart + ":" + (i - 1));
+                if (intervalMax > supMax) {
+                    supMax = intervalMax;
+                    bestStart = interStart;
+                    bestEnd = i - 1;
+                }
+                intervalMax = 0;
+                interStart = i + 1;
+                zero = true;
             }
         }
-
+        System.out.println("");
+        // พิมพ์ค่า SupremeMax และช่วงอาเรย์ที่มี SupremeMax
+        System.out.println(supMax);
+        // System.out.println(bestStart+" "+bestEnd);
+        for (int i = bestStart; i <= bestEnd; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
